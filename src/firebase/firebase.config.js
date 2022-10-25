ee67bbacb2da811a6840c9891682422d33fe90c4
Imran-Asad-06 <imran.asad.0607@gmail.com>
2022-10-25 16:33:16 +0600

    router set up done

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBJ436KjxdeLGqLUmdoU1zbNuwfzZB3n2k",
+  authDomain: "online-learning-platform-876bf.firebaseapp.com",
+  projectId: "online-learning-platform-876bf",
+  storageBucket: "online-learning-platform-876bf.appspot.com",
+  messagingSenderId: "587903316654",
+  appId: "1:587903316654:web:1ac6852b782e2528f9b33f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
